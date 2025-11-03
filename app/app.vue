524075e41faa720ce0ae47813384ea778db265ed
Ilyas Turki <turki.ilyass@gmail.com>
2025-11-03 16:54:47 +0100

    feat: custom app favicon

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,22 @@
 import ShortcutsDialog from '~/components/shortcuts-dialog.vue'
 import { useShortcut } from '~/composables/use-shortcuts'
 
+useHead({
+    titleTemplate: (titleChunk) => {
+        return titleChunk ? `${titleChunk} - Safi` : 'Safi'
+    },
+    htmlAttrs: {
+        lang: 'en',
+    },
+    link: [
+        {
+            rel: 'icon',
+            type: 'image/svg+xml',
+            href: '/favicon.svg',
+        },
+    ],
+})
+
 const isShortcutsOpen = useState('isShortcutsOpen', () => false)
 
 useShortcut('show-shortcuts', () => {
